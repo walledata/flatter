16f5266ce9fed51c4f05eacd457e8ddf0470a537
Steve Wang <steve235lab@hotmail.com>
2024-02-19 14:14:13 +0800

    Initial commit.

---
+++ flatter.py
@@ -0,0 +1,58 @@
+import glob
+import os
+import shutil
+import hashlib
+import json
+import argparse
+
+
+def generate_file_hash(filepath):
+    # 生成文件hash
+    hash_function = hashlib.sha256()
+    with open(filepath, 'rb') as f:
+        while chunk := f.read(8192):
+            hash_function.update(chunk)
+    return hash_function.hexdigest()
+
+
+def move_images_to_top_level(start_dir, dest_dir, extensions=('.jpg', '.png', '.gif')):
+    # 转换为绝对路径
+    start_dir = os.path.abspath(start_dir)
+    dest_dir = os.path.abspath(dest_dir)
+    # 创建目标路径
+    os.makedirs(dest_dir, exist_ok=True)
+    new_filename_original_path_map = {}
+    for ext in extensions:
+        for filepath in glob.glob(start_dir + '/**/*' + ext, recursive=True):
+            directory, filename = os.path.split(filepath)
+            # 计算图片的hash作为新的文件名
+            new_filename = generate_file_hash(directory + '/' + filename) + ext
+            # 记录文件原始的路径与新文件名的映射关系
+            new_filename_original_path_map.update({new_filename: directory + '/' + filename})
+            shutil.copy(filepath, dest_dir + "/" + new_filename)
+    return json.dumps(new_filename_original_path_map)
+
+
+def move_images_back(dest_dir, mapping_file_path):
+    new_filename_original_path_map = json.load(open(mapping_file_path, 'r'))
+    for new_filename, original_path in new_filename_original_path_map.items():
+        # 创建文件原始路径的目录结构
+        os.makedirs(os.path.dirname(original_path), exist_ok=True)
+        # 把文件放回原位置
+        shutil.copy(dest_dir + new_filename, original_path)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='图片资源目录展平与复原 | 展平示例: flatter flat ./tiantianbaiyin/ ./target/ > map.json | 还原示例: flatter move_back map.json ./target/')
+    parser.add_argument('action', choices=['flat', 'move_back'], help='flat - 展平, move_back - 还原')
+    parser.add_argument('start_dir', help='展平操作：原始图片资源目录；还原操作：记录原始路径的json文件')
+    parser.add_argument('dest_dir', help='存放展平后图片的目录')
+    args = parser.parse_args()  # 解析命令行参数
+    if args.action == 'flat':
+        print(move_images_to_top_level(args.start_dir, args.dest_dir))
+    elif args.action == 'move_back':
+        move_images_back(args.dest_dir, args.start_dir)
+
+
+if __name__ == "__main__":
+    main()
